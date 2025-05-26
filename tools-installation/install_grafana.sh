a6bc5a7c9968cf726b37b3406000c675dc8f9ce5
satyamsingh877 <78309503+satyamsingh877@users.noreply.github.com>
2025-05-27 01:34:13 +0530

    Delete tools-installation directory

--- tools-installation/install_grafana.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-echo "Installing Grafana..."
-helm repo add grafana https://grafana.github.io/helm-charts
-helm repo update
-kubectl create namespace grafana
-helm install grafana grafana/grafana --namespace grafana
-echo "Grafana installed in the grafana namespace"
--- tools-installation/install_prometheus.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-echo "Installing Prometheus..."
-helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
-helm repo update
-kubectl create namespace monitoring
-helm install prometheus prometheus-community/prometheus --namespace monitoring
-echo "Prometheus installed in the monitoring namespace"
